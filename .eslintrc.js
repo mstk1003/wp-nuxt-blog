78d0d455d97a896110264c5464b9a51cdcc920d3
Sato Masataka <masataka@SatonoMacBook-Air.local>
2022-01-03 15:48:30 +0900

    eslint設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,18 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
+    jest:true
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    sourceType: 'module'
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/recommended', // Use this if you are using Vue.js 2.x.
-    '@nuxtjs'
+    'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
-  parser: 'vue-eslint-parser',
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
